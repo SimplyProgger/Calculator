22930b21feb55c0fa13c3d53291a8255b1f4d5fe
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-08-16 14:21:17 +0500

    added function for determining the first number

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 # Calculator by SimplyProgger. Version 0.0
+
+line = str(input())
+
+total = 0
+count_mark = 0
+previous_mark = 0
+count = 0
+
+
+for symbol in line:
+	plus = False
+	minus = False
+	multiply = False
+	divide = False
+	try:
+		number = int(symbol)
+		previous_mark += 1
+	except ValueError:
+		print(symbol)
+		if count_mark == 0:
+			total = int(line[:count])
+			count_mark += 1
+
+	count += 1
+
+
+
+print(total)
